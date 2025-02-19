1623eb86e9797781e7326baff505d2b4f9cd60f6
slyncrafty <slyncraftyy@gmail.com>
2025-02-19 22:22:03 +0000

    Added solutions

--- C21/class21-materials/alien-rangers-review/js/main.js
+++ C21/class21-materials/alien-rangers-review/js/main.js
@@ -1,9 +1,28 @@
 //Arrays
 
 //Create and array of tv shows. Loop through and print each show to the console
+const tvArray = ["alien", "rangers", "review", "happy"];
+tvArray.forEach(x => console.log(x));
 
 //Create and array of numbers
+const numArray = [];
+for(let i = 0; i < 5; i++)
+{
+    numArray.push(Math.floor(Math.random()*50));
+}
+numArray.forEach(x => console.log( x ));
 //Return a new array of numbers that includes every even number from the previous Arrays
+const evenArray = [];
+numArray.forEach(x => x%2 === 0? evenArray.push(x) : null);
+evenArray.forEach(x => console.log(`evenArray: ${x}`));
 
 //Create a function that takes in an array of numbers
 //Alert the sum of the second lowest and the second highest number
+function sumOfSecondLowAndHigh(arr){
+    arr = arr.sort((a, b) => a - b);
+    let low = arr[1];
+    let high = arr[arr.length - 2]
+    console.log( low, high, `sum: ${low + high}`);
+}
+const arrr = [ 123, 23, 23, 434, 22, 1, 3 ]
+sumOfSecondLowAndHigh(arrr);
--- C21/class21-materials/mighty-morphin-review/js/main.js
+++ C21/class21-materials/mighty-morphin-review/js/main.js
@@ -1,18 +1,43 @@
 // *Variables*
 // Declare a variable, reassign it to your fav holiday, make sure it is in all caps, and print the value to the console
 
-//Declare a variable, assign it a string, alert the last three characters in the string (Use your google-fu and the MDN)
+let favHoliday;
+favHoliday = "Christmas".toUpperCase();
+console.log( favHoliday );
 
+//Declare a variable, assign it a string, alert the last three characters in the string (Use your google-fu and the MDN)
+let str;
+str = "google-fu"
+alert( str.slice(-3) )
 // *Functions*
 // Create a function that takes in 5 numbers. Subtract all five from 100. Alert the absolute value of the difference. Call the function.
 
+function take5Subtract100(a,b,c,d,e){
+    alert( Math.abs(100 - a - b - c - d - e) );
+}
+take5Subtract100(5,-10,15,20,100);
 
 // Create a function that takes in 3 numbers. Console log lowest and highest values. Call the function.
-
+function take3(a,b,c){
+    console.log( `min value: ${Math.min(a,b,c,)} and max value: ${Math.max(a,b,c)}` );
+}
+take3(3,5,10);
 
 // *Conditionals*
 //Create a function that returns heads or tails randomly and as fairly as possible. Call the function.
-
+// function headsOrTails(){
+//     return Math.random(0,1) < 0.5 ? "heads" : "tails";
+// }
+const headsOrTails = () => Math.random() < 0.5 ? "heads" : "tails";
+console.log( "Heads? or Tails? ", headsOrTails() );
 
 //*Loops*
 //Create a function that takes in a number. Console log the result of heads or tails using the previous function x times where x is the number passed into the function. Call the function.
+function headsOrTailsLoop(a) {
+    for(let i = 0; i < a; i++)
+    {
+        console.log("Heads? or Tails? ", i, headsOrTails());
+    }
+}
+
+headsOrTailsLoop(3);
