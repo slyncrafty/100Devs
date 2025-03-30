4b2bd332f0785d27418d7bf951be592d29ba48dc
slyncrafty <101752232+slyncrafty@users.noreply.github.com>
2025-03-30 00:04:29 +0000

    Added C27 -utilizing localStorage to create app saving bot score

--- C27/local-storage-botScore-button/js/main.js
+++ C27/local-storage-botScore-button/js/main.js
@@ -1,3 +1,13 @@
-//Create a button that adds 1 to a botScore stored in localStorage 
+//Create a button that adds 1 to a botScore stored in localStorage
+if(localStorage.getItem('botScore')) 
+    document.querySelector('h2').textContent = localStorage.getItem('botScore');
+else localStorage.setItem('botScore', 0);
 
+document.querySelector('button').addEventListener('click', addAnotherOne)
 
+function addAnotherOne(){
+    let botScoreVal =+ localStorage.getItem('botScore') + 1;
+    localStorage.setItem('botScore', botScoreVal);
+    document.querySelector('h2').textContent = botScoreVal;
+    // console.log(botScoreVal)
+}
