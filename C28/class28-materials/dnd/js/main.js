9571b9293c3d0d312dba4943b7c4319dfb401a72
slyncrafty <101752232+slyncrafty@users.noreply.github.com>
2025-05-15 18:12:31 +0100

    Added DND API fetch to populate HTML contents

--- C28/class28-materials/dnd/js/main.js
+++ C28/class28-materials/dnd/js/main.js
@@ -1,17 +1,31 @@
-//Example fetch using DnD5eAPI - place subclasses in ul
+//Example fetch using DnD5eAPI(https://www.dnd5eapi.co/api) - place subclasses in ul
 document.querySelector('button').addEventListener('click', getFetch)
+document.querySelector('input').addEventListener('keydown', (e) => {
+    if(e.key === 'Enter') getFetch();
+} );
 
 function getFetch(){
   const choice = document.querySelector('input').value
+  console.log(choice);
   const url = `https://www.dnd5eapi.co/api/spells/${choice}`
-
   fetch(url)
       .then(res => res.json()) // parse response as JSON
       .then(data => {
-       
+       console.log(data)
+       setDetails(data);
       })
       .catch(err => {
           console.log(`error ${err}`)
       });
 }
 
+function setDetails(data) {
+    document.querySelector('h3').textContent = data.classes.map(t=>t.name).join(' / ');
+    const subclasses = document.querySelector('ul');
+    subclasses.innerHTML = '';
+    data.subclasses.forEach((elem) =>  {
+        const li = document.createElement('li');
+        li.textContent = elem.name;
+        subclasses.appendChild(li);
+    });
+}
