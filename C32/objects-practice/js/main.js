64237e4291be2a84a34f9e3f75daf2587be69319
slyncrafty <101752232+slyncrafty@users.noreply.github.com>
2025-06-02 14:49:33 +0100

    Added in class Solutions C32 create a parent class and children classes to demonstrate encapsulation, abstraction, inheritance, and polymorphism

--- C32/objects-practice/js/main.js
+++ C32/objects-practice/js/main.js
@@ -2,50 +2,66 @@
 // Extend that parent class into two children
 // Use Encapsulation, Abstraction, Inheritance, and Polymorphism 
 class Contractor{
-    constructor(name,role){
-        this._name = name
-        this._role = role
+    constructor(name, role){
+        this._name = name;
+        this._role = role;
     }
     get name(){
-        return this._name
+        return this._name;
     }
     get role(){
-        return this._role
+        return this._role;
+    }
+    getGreeting() {
+        return `Hello, my name is ${this.name}, and I am on the ${this._role} team!`;
     }
     sayHello(){
-        console.log(`Hello, I am on the ${this._role} team at #100Devs!`)
+        console.log(this.getGreeting())
+    }
+    sayBye(){
+        console.log("It's been a pleasure working with you")
     }
 }
+
 class Front extends Contractor{
-    constructor(name,role,tech){
-        super(name,role)
-        this._tech = tech
+    constructor(name, role, tech) {
+        super(name, role);
+        this._tech = tech;
     }
     get tech(){
-        return this._tech
+        return this._tech;
     }
-    sayHello(){
-        console.log(`Hello, I am on the ${this._role} team at #100Devs and I use ${this._tech}`)
+
+    getGreeting() {
+        return `${super.getGreeting()} --specifically working on ${this.tech}`;
     }
 }
+
 class Back extends Contractor{
-    constructor(name,role,tech){
-        super(name,role)
-        this._tech = tech
+    constructor(name, role, tech) {
+        super(name, role);
+        this._tech = tech;
     }
     get tech(){
-        return this._tech
+        return this._tech;
     }
-    sayHello(){
-        console.log(`Hello, I am on the ${this._role} team at #100Devs and I use ${this._tech}`)
+    
+    getGreeting() {
+        return `${super.getGreeting()} --specifically working on ${this.tech}`;
     }
 }
-let bob = new Contractor('Bob','Front-end')
-let simba = new Front('Simba','Front-end','React')
-let machi = new Back('The Machine','Back-end','Node')
 
-let agency = [bob,simba,machi]
+// let bob = new Contractor('Bob','Front-end')
+// let machi = new Front('The Machine','Front-end','React')
+// let simba = new Back('Simba','Back-end','Node')
 
-for(person of agency){
-    person.sayHello()
-}
+// let agencyList = [bob,machi,simba]
+
+// // polymorphism
+// for(person of agencyList){
+//     person.sayHello()
+// }
+
+
+let machi = new Front('The Machine','Front-end','React')
+machi.sayHello();
